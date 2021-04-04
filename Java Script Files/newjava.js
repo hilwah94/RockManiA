db28b94044bd942707f0c8896c45905dc74d09b9
hilwa94 <helwaomar@gmail.com>
2021-04-04 15:57:29 +0300

    add new

---
+++ Java Script Files/newjava.js
@@ -0,0 +1,13 @@
+
+
+Let userName = prompt('what\s your name?');
+console.log('userName');
+
+Let userAge = prompt('what\s your age?');
+console.log('userAge');
+
+Let favBand = prompt('what\s your name?');
+console.log('favBand');
+
+Let userSkill = prompt('do you play any instrument?');
+console.log('uuserSkill');
